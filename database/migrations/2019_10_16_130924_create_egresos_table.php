32bb8783509b2179e53ff6c3881c9ffdd6754f9f
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2019-10-16 08:32:04 -0500

    Avance Egreso bodega

---
+++ database/migrations/2019_10_16_130924_create_egresos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEgresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ENF_EGRESOS', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('codigo')->nullable();
+            $table->date('fecha');
+            $table->string('semana');
+            $table->integer('idhacienda');
+            $table->integer('idempleado');
+            $table->float('total');
+            $table->float('saldo');
+            $table->binary('status');
+            $table->string('user')->nullable();
+            $table->string('pcID')->nullable();
+            $table->timestamps();
+
+            $table->foreign('idhacienda')
+                ->references('id_hac')->on('SIS_HACIENDA');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('egresos');
+    }
+}
---
+++ database/migrations/2019_10_16_130957_create_detegresos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetegresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ENF_DET_EGRESOS', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('codigo')->nullable();
+            $table->integer('id_egreso');
+            $table->date('fecha');
+            $table->integer('idbodega')->nullable();
+            $table->integer('idmaterial');
+            $table->float('cantidad');
+            $table->binary('presente')->nullable();
+            $table->binary('futuro')->nullable();
+            $table->binary('status')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_egreso')
+                ->references('id')->on('ENF_EGRESOS');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detegresos');
+    }
+}
