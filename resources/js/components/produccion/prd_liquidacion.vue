01e02729a8f4fd5428c719ff15bd20e7db13336d
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2019-11-22 13:19:29 -0500

    liquidacion

--- resources/js/components/produccion/prd_liquidacion.vue
+++ resources/js/components/produccion/prd_liquidacion.vue
@@ -7,28 +7,33 @@
                     <th scope="col">Descripcion</th>
                     <th scope="col">Ubicacion</th>
                     <th scope="col">Tipo de Caja</th>
-                    <th scope="col">#Cajas</th>
-                    <th scope="col">Tarifa</th>
-                    <th scope="col">Monto</th>
+                    <th scope="col" class="text-center">#Cajas</th>
+                    <th scope="col" class="text-center">Tarifa</th>
+                    <th scope="col" class="text-center">Monto</th>
                 </tr>
                 </thead>
                 <tbody class="table-sm">
                 <template v-for="(liquidacion, index) in liquidaciones">
                     <tr class="table-active">
-                        <td colspan="4">{{liquidacion[0]}}</td>
-                        <td>{{liquidacion[1]}}</td>
-                        <td>{{liquidacion[2]}}</td>
+                        <td colspan="1"><b>{{liquidacion[0]}}</b></td>
+                        <td colspan="1"><b>{{liquidacion[1]}}</b></td>
+                        <td colspan="4"><b>{{liquidacion[2]}}</b></td>
                     </tr>
                     <template v-for="(cajas, index) in liquidacion['Detalle']">
                         <tr>
                             <td>{{cajas[0]}}</td>
                             <td>{{cajas[1]}}</td>
                             <td>{{cajas[2]}}</td>
-                            <td>{{cajas[4]}}</td>
-                            <td>{{cajas[5]}}</td>
-                            <td>{{cajas[7]}}</td>
+                            <td class="text-center"><b>{{cajas[4] = parseInt(cajas[4])}}</b></td>
+                            <td class="text-center">$ {{cajas[5] = parseFloat(cajas[5].split("$")[1]).toFixed(3)}}</td>
+                            <td class="text-center"><b>$ {{cajas[7] = parseFloat(cajas[4] * cajas[5]).toFixed(2)}}</b>
+                            </td>
                         </tr>
                     </template>
+                    <tr class="table-success table-sm">
+                        <td colspan="5"><h5 class="mb-0"><b>TOTAL</b></h5></td>
+                        <td colspan="1" class="text-center"><h5 class="mb-0"><b>$ {{parseFloat(totalMonto(liquidacion)).toFixed(2)}}</b></h5></td>
+                    </tr>
                 </template>
                 </tbody>
             </table>
@@ -54,7 +59,17 @@
         },
         mounted(){
             var self = this;
-            console.log(this.liquidaciones);
+            //console.log(this.liquidaciones);
+        },
+        methods: {
+            totalMonto: function (values) {
+                return values['Detalle'].reduce((acc, val) => {
+                    return acc + parseFloat(val[7]);
+                }, 0);
+            },
+            total: function(values){
+                return values.reduce
+            }
         }
     }
 </script>
